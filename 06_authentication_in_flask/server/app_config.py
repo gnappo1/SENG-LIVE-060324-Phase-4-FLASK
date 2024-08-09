173b7ceabd5b7387098bb8a83e5ffc32271ab27d
Matteo Piccini <gnappo.1@live.it>
2024-08-09 13:35:49 -0700

    fix flask-session issue

--- 06_authentication_in_flask/server/app_config.py
+++ 06_authentication_in_flask/server/app_config.py
@@ -8,14 +8,15 @@ from flask_session import Session
 from os import environ
 
 app = Flask(__name__)
+# flask-sqlalchemy connection to app
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///theater.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SQLALCHEMY_ECHO"] = True
-# app.config["SESSION_TYPE"] = "sqlalchemy"
+app.config["SESSION_TYPE"] = "sqlalchemy"
+db = SQLAlchemy(app)
+app.config["SESSION_SQLALCHEMY"] = db
 
 app.secret_key = environ.get("SESSION_SECRET")
-# flask-sqlalchemy connection to app
-db = SQLAlchemy(app)
 # flask-migrate connection to app
 migrate = Migrate(app, db)
 # flask-restful connection to app
@@ -25,4 +26,4 @@ cors = CORS(app)
 # flask-bcrypt configuration
 flask_bcrypt = Bcrypt(app)
 # flask-session configuration
-# Session(app)
+Session(app)
